c4d6f874fcd8b1cfe18647f404ad27af87c75f1e
Lutfi1919 <luthfiamad19@gmail.com>
2026-07-30 11:59:55 +0700

    feat: add necessary endpoints

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -5,3 +5,23 @@ export const get = async (): Promise<Transaction[]> => {
   const response = await api.get<Transaction[]>("/");
   return response.data;
 };
+
+export const show = async (): Promise<Transaction[]> => {
+  const response = await api.get<Transaction[]>("/transaction/my_transactions");
+  return response.data;
+};
+
+export const create = async (transaction: Transaction[]): Promise<Transaction[]> => {
+  const response = await api.post<Transaction[]>("/transaction/create", transaction)
+  return response.data;
+}
+
+export const update = async (transaction: Transaction[]): Promise<Transaction[]> => {
+  const response = await api.put<Transaction[]>("/transaction/update", transaction)
+  return response.data;
+}
+
+export const destroy = async (id: number): Promise<void> => {
+  const response = await api.delete(`/transaction/delete/${id}`);
+  return response.data;
+}
