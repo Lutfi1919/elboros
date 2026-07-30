e8be6c639308599b18a3aa7f1e8094ed4e4562aa
Lutfi1919 <luthfiamad19@gmail.com>
2026-07-30 11:08:29 +0700

    feat: add bearer token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,4 +4,14 @@ const api = axios.create({
     baseURL: "http://localhost:3000"
 });
 
-export default api
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+
+    if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+})
+
+export default api;
