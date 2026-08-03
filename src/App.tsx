2da9e153f64872b67da398c9d0fb5997acc23da2
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-03 10:32:41 +0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import ParentComponent from './components/ParentComponent';
+import ChildrenProps from './components/ChildrenProps';
+import ListItem from './components/ListItem';
 
 function App() {
   return (
     <>
-      <div className="">
-        
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <ParentComponent />
+        <hr />
+        <ChildrenProps>lorem</ChildrenProps>
+        <ListItem />
       </div>
     </>
   )
