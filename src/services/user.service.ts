ffca747ddb97bbc6d113da6841cc2d76323b29f9
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-03 16:43:20 +0700

    refactor: refactor promises

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,23 @@
 import api from "../api/axios";
-import type { User } from "../types/User";
+import type { LoginPayload, LoginResponse } from "../types/Login";
+import type { User, UserResponse } from "../types/User";
 
-export const login = async (user: User[]): Promise<User[]> => {
-    const response = await api.post<User[]>("/login", user);
+export const login = async (user: LoginPayload): Promise<LoginResponse> => {
+    const response = await api.post<LoginResponse>("/login", user);
     return response.data;
 }
 
-export const signup = async (user: User[]): Promise<User[]> => {
-    const response = await api.post<User[]>("/signup", user);
+export const signup = async (user: User): Promise<User> => {
+    const response = await api.post<User>("/signup", user);
     return response.data;
 }
 
-export const saldoUpdate = async (user: User[]): Promise<User[]> => {
-    const response = await api.put<User[]>(`/saldo`, user);
+export const saldoUpdate = async (user: User): Promise<User> => {
+    const response = await api.put<User>("/saldo", user);
+    return response.data;
+}
+
+export const showUser = async (): Promise<UserResponse> => {
+    const response = await api.get<UserResponse>("/show_user");
     return response.data;
 }
