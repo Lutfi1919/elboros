0774e57fe247aee32fd10d5f5dcc8d17433642d2
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-03 11:28:05 +0700

    feat: add logout function

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,24 @@ import './App.css'
 import ParentComponent from './components/ParentComponent';
 import ChildrenProps from './components/ChildrenProps';
 import ListItem from './components/ListItem';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
+  const logout = (event: React.MouseEvent<HTMLButtonElement>) => {
+    localStorage.removeItem("token")
+
+    console.log(event.target);
+
+    navigate("/login")
+  }
+
   return (
     <>
+      <button className='m-2 p-2 rounded-2xl bg-red-500 cursor-pointer' onClick={logout}>
+        Logout
+      </button>
       <div className="min-h-screen flex flex-col items-center justify-center">
         <ParentComponent />
         <hr />
