d5017f0e87e14b328022ec7babbf72c9d1867271
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-05 16:30:40 +0700

    style: add some style to the wrapper

--- src/components/ListTransactionCard.tsx
+++ src/components/ListTransactionCard.tsx
@@ -2,13 +2,15 @@ import TransactionCard from "./TransactionCard";
 import type { TransactionResponse } from "../types/Transaction";
 
 export default function ListTransactionCard({ data }: TransactionResponse) {
-  return (
-    <>
-        <div className="">
-            {data.map((item) => (
-                <TransactionCard item={item} />
-            ))}
-        </div>
-    </>
-  );
+    const today = new Date().toLocaleDateString("id-ID", { weekday: "long", year: "numeric", month: "long", day: "numeric" }); 
+    return (
+        <>
+            <div className="border shadow-xl border-gray-200 rounded-xl p-5">
+                <p className="font-semibold text-lg mb-3">{today}</p>
+                {data.map((item) => (
+                    <TransactionCard item={item} />
+                ))}
+            </div>
+        </>
+    );
 }
