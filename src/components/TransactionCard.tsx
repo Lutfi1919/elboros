74e08f626a27ad36c8edc1f3d11f2f22a00823cd
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-05 16:29:53 +0700

    style: styling the card

--- src/components/TransactionCard.tsx
+++ src/components/TransactionCard.tsx
@@ -7,10 +7,10 @@ type Props = {
 export default function TransactionCard({item} :Props) {
     return (
         <>
-            <div className="ring ring-inset ring-gray-300 rounded-xl p-3 mb-2 w-120 flex justify-between items-center">
-                <p className="text-xl capitalize">{item.judul}</p>
-                <p className="opacity-70">{item.catatan}</p>
-                <p className="font-bold">Rp {item.nominal.toLocaleString()}</p>
+            <div className="border border-gray-200 rounded-xl p-3 mb-2 grid grid-cols-3 items-center">
+                <p className="capitalize">{item.judul}</p>
+                <p className="font-light capitalize text-sm opacity-60">{item.catatan}</p>
+                <p className=" text-end">Rp {item.nominal.toLocaleString()}</p>
             </div>
         </>
     )
