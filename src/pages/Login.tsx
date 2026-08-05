b8b60fb9d3d5e409b13ba86500fdcb72cb0a996c
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-05 15:44:13 +0700

    feat: add error message

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { login } from "../services/user.service";
 import { useNavigate } from "react-router-dom";
 import  Swal from 'sweetalert2';
@@ -6,6 +6,7 @@ import  Swal from 'sweetalert2';
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -17,7 +18,7 @@ export default function Login() {
       localStorage.setItem("token", data.data.token);
 
       navigate("/");
-      
+
       Swal.fire({
         title: "Berhasil Login",
         text: "Login berhasil",
@@ -25,14 +26,24 @@ export default function Login() {
       })
 
     } catch (error) {
-      console.error(error);
+      setError("Pastikan mengisi email dan password dengan benar!")
     }
   };
+
+  useEffect(() => {
+    if (error) {
+        const timer = setTimeout(() => {
+            setError("");
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   return (
     <>
       <div className="min-h-screen flex items-center justify-center">
-        <div className="w-120 p-6 rounded-lg shadow-sm bg-white hover:-translate-y-1 duration-300">
+        <div className="w-120 p-6 rounded-lg shadow-sm bg-white">
           <div>
             <form onSubmit={handleSubmit}>
               <div className="space-y-4">
@@ -60,10 +71,15 @@ export default function Login() {
 
                 <button
                   type="submit"
-                  className="w-full bg-blue-600 text-white py-2 rounded"
+                  className="w-full bg-blue-600 text-white py-2 rounded hover:ring hover:ring-inset hover:ring-blue-600 hover:text-blue-600 hover:bg-transparent transition-all duration-150 cursor-pointer"
                 >
                   Login
                 </button>
+                <div className={`overflow-hidden transition-all duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)] ${error ? "max-h-20 opacity-100 translate-y-0 mt-3" : "max-h-0 opacity-0 -translate-y-2"}`}>
+                    <div className="bg-red-50 border border-red-200 text-red-500 text-sm rounded-lg px-4 py-3">
+                        {error}
+                    </div>
+                </div>
               </div>
             </form>
           </div>
