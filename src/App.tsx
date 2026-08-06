5d499bcfd09c93eae6b5e7c7940c3a2a85c21336
Lutfi1919 <luthfiamad19@gmail.com>
2026-08-06 10:08:54 +0700

    feat: add something that i dont know

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
 import "./App.css";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { User } from "./types/User";
 import { showUser } from "./services/user.service";
 import type { Transaction } from "./types/Transaction";
-import { show } from "./services/transaction.service";
+import { create, show } from "./services/transaction.service";
 import Swal from "sweetalert2";
 import ListTransactionCard from "./components/ListTransactionCard";
 
 function App() {
   const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+  const [userTr, setUserTr] = useState<Transaction[]>([]);
+  const [form, setForm] = useState<Partial<Transaction>>({
+    judul: "",
+    catatan: "",
+    nominal: 0,
+  });
+
+  // jujur gua ga ngerti function ini🤣
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm(
+      (prev) =>
+        ({
+          ...prev,
+          [name]: name === "nominal" ? Number(value) : value,
+        }) as Partial<Transaction>,
+    );
+  };
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      await create(form as Transaction);
+
+      Swal.fire({
+        title: "Berhasil",
+        text: "Transaksi berhasil dibuat",
+        icon: "success",
+      });
+
+      setForm({ judul: "", catatan: "", nominal: 0 });
+
+      await getUserTransactions();
+    } catch (error) {
+      console.error(error);
+      Swal.fire("Error", "Gagal membuat transaksi", "error");
+    }
+  }
 
   const logout = () => {
     localStorage.removeItem("token");
@@ -23,15 +63,11 @@ function App() {
     });
   };
 
-  const [user, setUser] = useState<User>();
-  const [userTr, setUserTr] = useState<Transaction[]>([]);
-
   async function getUser() {
     try {
       const result = await showUser();
 
       setUser(result.data);
-
     } catch (error) {
       console.error(error);
     }
@@ -41,8 +77,15 @@ function App() {
     try {
       const result = await show();
 
-      setUserTr(result.data);
+      const todayTransactions = result.data.filter((transaction: Transaction) => {
+        const today = new Date();
+        const createdAt = new Date(transaction.createdAt ?? "");
 
+        return createdAt.toDateString() === today.toDateString();
+      });
+
+      setUserTr(todayTransactions);
+      
     } catch (error) {
       console.log(error);
     }
@@ -55,41 +98,68 @@ function App() {
 
   return (
     <>
-      <div className="px-10">
+      <div className="px-10 bg-[#f6f6f6] min-h-screen">
         <button
           className="m-2 px-5 py-2 rounded-lg text-white bg-red-500 cursor-pointer"
           onClick={logout}
         >
           Logout
         </button>
-        <ListTransactionCard data={userTr} />
 
-
-        <div className="">
-          <form>
+        <div className="grid grid-cols-2 gap-4">
+          <ListTransactionCard data={userTr} />
+          <form
+            onSubmit={handleSubmit}
+            className="border shadow-xl border-gray-200 rounded-xl p-5"
+          >
             <div className="mb-3">
-              <label htmlFor="judul" className="block">Judul</label>
-              <input type="text" id="judul" className="w-140 rounded px-2 py-1 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all duration-300" />
+              <label htmlFor="judul" className="block">
+                Judul
+              </label>
+              <input
+                name="judul"
+                type="text"
+                id="judul"
+                value={form.judul ?? ""}
+                onChange={handleChange}
+                className="w-140 rounded px-2 py-1 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all duration-300"
+              />
             </div>
             <div className="mb-3">
-              <label htmlFor="catatan" className="block">Catatan</label>
-              <input type="text" id="catatan" className="w-140 rounded px-2 py-1 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all duration-300" />
+              <label htmlFor="catatan" className="block">
+                Catatan
+              </label>
+              <input
+                name="catatan"
+                type="text"
+                id="catatan"
+                value={form.catatan ?? ""}
+                onChange={handleChange}
+                className="w-140 rounded px-2 py-1 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all duration-300"
+              />
             </div>
             <div className="">
-              <label htmlFor="nominal" className="block">Nominal</label>
-              <input type="number" id="nominal" className="w-140 rounded px-2 py-1 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all duration-300" />
+              <label htmlFor="nominal" className="block">
+                Nominal
+              </label>
+              <input
+                name="nominal"
+                type="number"
+                id="nominal"
+                value={form.nominal}
+                onChange={handleChange}
+                className="w-140 rounded px-2 py-1 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-all duration-300"
+              />
             </div>
 
-            <button type="submit" className="mt-3 px-5 py-2 rounded-lg text-white bg-blue-500 cursor-pointer">Submit</button>
+            <button
+              type="submit"
+              className="mt-3 px-5 py-2 rounded-lg text-white bg-blue-500 cursor-pointer"
+            >
+              Submit
+            </button>
           </form>
         </div>
-
-
-        <div className="min-h-screen flex flex-col items-center justify-center">
-          {user ? <p className="mb-4">Halo, {user.name}</p> : <p>Loading...</p>}
-          {user ? <p className="mb-4">Email: {user.email}</p> : <p>Loading...</p>}
-        </div>
-
       </div>
     </>
   );
